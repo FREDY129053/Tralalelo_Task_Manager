133c81958d8fff9da2e5395a4886117b92635ddb
FREDY129053 <83633741+FREDY129053@users.noreply.github.com>
2025-05-04 11:32:47 +1000

    Update users.py

--- backend/app/src/db/models/users.py
+++ backend/app/src/db/models/users.py
@@ -1,12 +1,15 @@
 from tortoise import fields, models
+import uuid
 from .boards import Comment, BoardUser
 
 class User(models.Model):
-  id = fields.UUIDField(pk=True)
+  id = fields.UUIDField(pk=True, default=uuid.uuid4)
   username = fields.CharField(max_length=150)
   email = fields.CharField(max_length=255, unique=True)
   phone = fields.CharField(max_length=20, null=True)
   avatar_url = fields.CharField(max_length=255, null=True)
+  is_admin = fields.BooleanField(default=False)
+  password = fields.CharField(max_lenght=255, default='')
 
   boards: fields.ReverseRelation["BoardUser"]
   comments: fields.ReverseRelation["Comment"]
